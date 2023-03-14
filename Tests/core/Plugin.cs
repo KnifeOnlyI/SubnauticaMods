48825190895d7fe39ca54396e676d66a6a30fac4
KnifeOnlyI <dany.pignoux@outlook.fr>
2023-03-14 19:00:29 +0100

    Initial commit

---
+++ Tests/core/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace fr.koi.tests.core
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class TestsPlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "KOI_Tests";
+        private const string PluginName = "KOI Tests";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public new static ManualLogSource Logger { get; private set; }
+
+        private void Awake()
+        {
+            Harmony.PatchAll();
+
+            Logger = base.Logger;
+
+            Logger.LogInfo(PluginName + " " + VersionString + " " + "loaded.");
+        }
+    }
+}
