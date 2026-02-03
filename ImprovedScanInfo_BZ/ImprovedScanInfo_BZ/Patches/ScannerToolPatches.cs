54eaeedbdb9edcfa0708f3c01931c7f308d2aa5f
call_me_xale <alexmheinz@gmail.com>
2026-02-02 17:22:36 -0800

    ImprovedScanInfo_BZ: fixed hint text appearing when no item name is visible
    Includes minor refactoring to reduce duplicate code.

---
+++ ImprovedScanInfo_BZ/ImprovedScanInfo_BZ/Patches/ScannerToolPatches.cs
@@ -0,0 +1,40 @@
+using HarmonyLib;
+using Koi.Subnautica.ImprovedScanInfo_BZ.Utility;
+
+namespace Koi.Subnautica.ImprovedScanInfo_BZ.Patches
+{
+    [HarmonyPatch(typeof(ScannerTool))]
+    internal static class ScannerToolPatches
+    {
+        /// <summary>
+        /// Harmony patch applied to: <c>ScannerTool.OnHover()</c>
+        /// <para>
+        /// Adds hint text to the player's reticle if the player is holding a powered scanner tool and looking at a
+        /// fragment of an already-scanned blueprint.
+        /// </para>
+        /// </summary>
+        /// <param name="___energyMixin">Injected: the power status of the scanner tool the player is holding</param>
+        [HarmonyPatch("OnHover"), HarmonyPostfix]
+        internal static void OnHover(EnergyMixin ___energyMixin, ScannerTool.ScanState ___stateCurrent)
+        {
+            if (___energyMixin.charge <= 0f) return;
+
+            PDAScanner.ScanTarget target = PDAScanner.scanTarget;
+
+            if (!target.IsValidScanTarget() || !target.IsScanCompleteFragment()) return;
+
+            HandReticle.main.SetText(
+                HandReticle.TextType.HandSubscript,
+                ModConstants.Translations.Keys.BlueprintAlreadySynthesized,
+                translate: true);
+        }
+
+        /// <summary>
+        /// Determines whether the specified scanner-tool target is valid and scannable.
+        /// </summary>
+        /// <param name="scanTarget">Any scanner-tool target</param>
+        /// <returns><c>true</c> if the target is valid and allows scanning; <c>false</c> otherwise</returns>
+        private static bool IsValidScanTarget(this PDAScanner.ScanTarget scanTarget) =>
+            scanTarget.isValid && (PDAScanner.CanScan(scanTarget) == PDAScanner.Result.Scan);
+    }
+}
